a49a81a6f76afc897646f697b054dcb442c9fe05
Donata Petkutė <90028851+pdonata6@users.noreply.github.com>
2021-10-15 15:17:07 +0300

    Delete zodziu_kiekis.cpp

--- zodziu_kiekis.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by Admin on 2021-10-15.
-//
-#include "studentu_duom.h"
- int zodziu_kiekis(string const& str) {
-    stringstream stream(str);
-    return distance(istream_iterator<string>(stream), istream_iterator<string>());
-}
